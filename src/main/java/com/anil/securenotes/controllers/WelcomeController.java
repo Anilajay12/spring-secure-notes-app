7d084e5e2628d928a0a07cad7ab49a73bc591669
Anil <anilkumar7062000@gmail.com>
2024-12-21 14:22:56 +0530

    Security With Method Level - PreAuthorize, Secured Annotation

--- src/main/java/com/anil/securenotes/controllers/WelcomeController.java
+++ src/main/java/com/anil/securenotes/controllers/WelcomeController.java
@@ -2,6 +2,8 @@ package com.anil.securenotes.controllers;
 
 
 import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
     @GetMapping(value = "/admin")
+//    @PreAuthorize("hasRole('ADMIN')")
+    @Secured({"ROLE_ADMIN"})
     public String admin() {
         return "admin";
     }
 
 
     @GetMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasRole('USER')")
     public String user() {
         return "user";
     }
